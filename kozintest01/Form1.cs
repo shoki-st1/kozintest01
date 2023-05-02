31ece413fd55589e0556bb559f278fa398c204a1
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2023-05-02 14:51:09 +0900

    ボタンの表示

--- kozintest01/Form1.cs
+++ kozintest01/Form1.cs
@@ -17,11 +17,13 @@ namespace kozintest01
         //定義の宣言
         //追加ボタン,削除
         private Button AddButton,DeleteButton;
+
         //テキストボックス名前、優先度、期限
         private TextBox name,rank,finish;
 
         //時間表示ラベル
         private Label TimeDis;
+
         //タイマー
         Timer DateTimer;
 
@@ -42,38 +44,77 @@ namespace kozintest01
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //form指定
             this.Width = 500;
             this.Height = 600;
 
-            //formサイズの指定
+            //form拡大縮小の指定
             this.MaximumSize = new Size(600,700);
             this.MinimumSize = new Size(400, 500);
 
 
-
             //設定
-            //追加ボタン
-            this.AddButton = new Button();
+        //追加ボタン
+            AddButton = new System.Windows.Forms.Button();
+            AddButton.FlatStyle = FlatStyle.Flat;
+            AddButton.FlatAppearance.BorderSize = 0;
+
+            //マウスが通る、押す
+            AddButton.FlatAppearance.MouseOverBackColor = Color.WhiteSmoke;
+            AddButton.FlatAppearance.MouseDownBackColor = Color.WhiteSmoke;
+
             //オブジェクト名
             this.AddButton.Name = "Add";
             this.AddButton.Text = "追加";
             this.AddButton.Font = new Font("UTF-8",10);
             this.AddButton.TextAlign = ContentAlignment.TopCenter;
+
             //位置
-            this.AddButton.Location = ();
+            this.AddButton.Location = new Point(100,200);
+            //ボタン大きさ
+            this.AddButton.Size = new System.Drawing.Size(50,30);
+
+            //イベント
+            this.AddButton.Click += new EventHandler(this.AddButton_Click);
+            this.AddButton.Parent = this;
+            this.AddButton.BringToFront();
+            this.ResumeLayout(false);
 
 
-            //削除ボタン
-            this.DeleteButton = new Button();
-            //オブジェクト名
+        //削除ボタン
+            DeleteButton = new System.Windows.Forms.Button();
+            DeleteButton.FlatStyle = FlatStyle.Flat;
+            DeleteButton.FlatAppearance.BorderSize = 0;
+
+            DeleteButton.FlatAppearance.MouseOverBackColor= Color.Gray;
+            DeleteButton.FlatAppearance.MouseDownBackColor = Color.WhiteSmoke;
+
             this.DeleteButton.Name = "Delete";
             this.DeleteButton.Text = "削除";
-            this.AddButton.Font = new Font("UTF-8", 10);
-            this.AddButton.TextAlign = ContentAlignment.TopCenter;
-            this.DeleteButton.Location = ();
+            this.DeleteButton.Font = new Font("UTF-8", 10);
+            this.DeleteButton.TextAlign = ContentAlignment.TopCenter;
+            //位置
+            this.DeleteButton.Location = new Point(300,400);
+            //ボタン大きさ
+            this.DeleteButton.Size = new System.Drawing.Size(50,30);
 
-
+            this.DeleteButton.Click += new EventHandler(this.DeleteButton_Click);
+            this.DeleteButton.Parent = this;
+            this.DeleteButton.BringToFront();
+            this.ResumeLayout(false);
 
         }
+
+        private void AddButton_Click(object sender,EventArgs e)
+        {
+
+        }
+
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
